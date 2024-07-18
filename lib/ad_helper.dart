d73548e0e6143fcb1928c8aab8257a0ee95f7413
Hafız <ramizovhafiz@gmail.com>
2024-07-18 10:00:44 +0300

    ad feature comment out

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -1,15 +1,13 @@
 import 'dart:io';
 
 class AdHelper{
-
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-6229914235416908/6653390291';
+      return 'ca-app-pub-6229914235416908/7934310236';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-6229914235416908/3177179715';
+      return 'ca-app-pub-6229914235416908/7934310236';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
   }
-
 }
